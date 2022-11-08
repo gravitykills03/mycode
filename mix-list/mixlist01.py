1e7b8ab5ac4cfd0eb65c793bf7fa574ba3bf769e
gravitykills03 <david.allen.holman@gmail.com>
2022-11-08 23:00:37 +0000

    manipulating lists

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -17,3 +17,19 @@ iplist = [5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh"]
 
 # display only the IP addresses on the screen
 print("The IP addresses are: " + iplist[3] + ", and : " + iplist[4])
+
+#define wordbank list
+wordbank = ["indentation", "spaces"]
+
+#Name list
+tlgstudents= ["Aaron", "Andy", "Asif", "Brent", "Cedric", "Chris", "Cory", "Ebrima", "Franco", "Greg", "Hoon", "Joey", "Jordan", "JC", "LB", "Mabel", "Shon", "Pat", "Zach"]
+
+wordbank.append(4)
+
+num = int(input("Give me a number between 0 and 18:\n>"))
+
+student_name = tlgstudents[num]
+
+print(f"{student_name} always uses {wordbank[2]} {wordbank[1]} to indent.")
+
+
