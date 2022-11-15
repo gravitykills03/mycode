e7aa389331d1649ebab8f6e4b492bad212e23910
gravitykills03 <david.allen.holman@gmail.com>
2022-11-15 22:01:00 +0000

    Pulling in data from API with python standard library

---
+++ iss/requests-ride_iss.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import requests
+
+MAJORTOM = 'http://api.open-notify.org/astros.json'
+
+def main():
+    """runtime code"""
+
+    groundctrl = requests.get(MAJORTOM)
+
+    helmetson = groundctrl.json()
+
+    print("\n\nConverted Python data")
+    print(helmetson)
+
+    print('\n\nPeople in Space: ', helmetson['number'])
+    people = helmetson['people']
+    print(people)
+
+if __name__ == "__main__":
+        main()
