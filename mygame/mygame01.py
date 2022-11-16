b31a8d860245d2470633b65e0f474c14adf946a7
gravitykills03 <david.allen.holman@gmail.com>
2022-11-16 19:51:26 +0000

    Adding changes to my RPG

--- mygame/mygame01.py
+++ mygame/mygame01.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-"""Driving a simple game framework with
-   a dictionary object | Alta3 Research"""
+
+import sys
+import pyfiglet
+
+zelda = pyfiglet.figlet_format("ZELDA")
+gameover = pyfiglet.figlet_format("GAME OVER")
+keyart = pyfiglet.figlet_format("KEY")
+doorart = pyfiglet.figlet_format("DOOR")
+
 #change show Instructions to be more unique
 def showInstructions():
     """Show the game instructions when called"""
@@ -9,44 +16,208 @@ def showInstructions():
     RPG Game
     ========
     Commands:
-      go [direction]
-      get [item]
+      Directions = "up, down, left, right"
+      Keys = "yes, no"
+      Unlock Door = "yes, no"
     ''')
-#change show Status to be more unique
-def showStatus():
-    """determine the current status of the player"""
-    # print the player's current location
-    print('---------------------------')
-    print('You are in the ' + currentRoom)
-    # print what the player is carrying
-    print('Inventory:', inventory)
-    # check if there's an item in the room, if so print it
-    if "item" in rooms[currentRoom]:
-      print('You see a ' + rooms[currentRoom]['item'])
-    print("---------------------------")
 
 
 # an inventory, which is initially empty
 inventory = []
 
-# a dictionary linking a room to other rooms
-rooms = {
+# At the start of the game, show the instructions for the user
+showInstructions()
 
-            'Hall' : {
-                  'south' : 'Kitchen',
-                  'east' : 'Dining Room',
-                  'item' : 'key'
-                },
+class Map:
 
-            'Kitchen' : {
-                  'north' : 'Hall',
-                  'item' : 'monster'  
-                },
-            'Dining Room' : {
-                'west' : 'Hall'
-                }
+    def __init__(self, height, width, player_x, player_y, paths):
+        self.height = height
+        self.width = width
+        self.x = player_x
+        self.y = player_y
+        self.paths = paths
 
-         }
+    def move(self, direction):
+        if direction == "up":
+            if ((self.x, self.y -1), (self.x, self.y)) not in self.paths:
+                print('Direction is blocked, choose a different route!')
+            else:
+                self.y -= 1
+        if direction == "down":
+            if ((self.x, self.y), (self.x, self.y + 1)) not in self.paths:
+                print('Direction is blocked, choose a different route!')
+            else:
+                self.y += 1
+        if direction == "right":
+            if ((self.x, self.y), (self.x + 1, self.y)) not in self.paths:
+                print('Direction is blocked, choose a different route!')
+            else:
+                self.x += 1
+        if direction == "left":
+            if ((self.x - 1, self.y), (self.x, self.y)) not in self.paths:
+                print('Direction is blocked, choose a different route!')
+            else:
+                self.x -= 1
+    def print_map(self):
+            for y in range(0, self.height):
+
+                # print the yth row of rooms
+                for x in range(0, self.width):
+                    if self.x == x and self.y == y:
+                        sys.stdout.write("[x]")  # this is the player's room
+                    else:
+                        sys.stdout.write("[ ]")  # empty room
+                    # now see whether there's a path to the next room
+                    if ((x, y), (x + 1, y)) in self.paths:
+                        sys.stdout.write("-")
+                    elif ((x + 1, y), (x, y)) in self.paths:
+                        sys.stdout.write("-")
+                    else:
+                        sys.stdout.write(" ")
+                # now that we've written the rooms, draw paths to next row
+                print()  # newline
+                for x in range(0, self.width):
+                    sys.stdout.write(" ")  # spaces for above room
+                    if ((x, y), (x, y + 1)) in self.paths:
+                        sys.stdout.write("|  ")
+                    elif ((x, y + 1), (x, y)) in self.paths:
+                        sys.stdout.write("|  ")
+                    else:
+                        sys.stdout.write("   ")
+                print()
+
+            # first room with a key in it, collect the key or not
+            if (self.x, self.y) == keys[0]:
+                print(keyart)
+                grabKeys = input("\nThere is a key in this room! Do you want to grab it? (yes or no): ")
+                if grabKeys == "yes":
+                    print("You have collected a key!")
+                    inventory.append("key")
+                else:
+                    print("You can come back and grab the key later.")
+
+            # second room with a key, collect the key or not
+            if (self.x, self.y) == keys[1]:
+                print(keyart)
+                grabKeys = input("\nThere is a key in this room! Do you want to grab it? (yes or no): ")
+                if grabKeys == "yes":
+                    print("You have collected a key!")
+                    inventory.append("key")
+                else:
+                    print("You can come back and grab the key later.")
+
+            # if you come across a locked door, there are two of them
+            if ((self.x, self.y), (self.x, self.y + 1)) == lockedDoors[0]:
+                print(doorart)
+                unlockDoor = input("\nWould you like to open the locked door? You need a key to do so. (yes or no): ")
+                if unlockDoor == "yes" and len(inventory) == 0:
+                    print("You do not have a key to unlock the door! Please retrieve a key first!")
+                    self.y += 1
+                elif unlockDoor == "yes" and inventory[0]:
+                    inventory.remove("key")
+                    print("Door is unlocked! You may proceed!")
+                elif unlockDoor == "no":
+                    print("Door is still locked")
+                    self.y += 1
+
+            # second locked door
+            if ((self.x, self.y), (self.x - 1, self.y)) == lockedDoors[1]:
+                print(doorart)
+                unlockDoor = input("\nWould you like to open the locked door? You need a key to do so. (yes or no): ")
+                if unlockDoor == "yes" and len(inventory) == 0:
+                    print("You do not have a key to unlock the door! Please retrieve a key first!")
+                    self.x += 1
+                elif unlockDoor == "yes" and inventory[0]:
+                    inventory.remove("key")
+                    print("Door is unlocked! You may proceed!")
+                elif unlockDoor == "no":
+                    print("Door is still locked")
+                    self.x += 1
+            # battling the boss
+#            if (self.x, self.y) == boss[0]:
+#                while True:
+#                    bossfight = {
+#                            "Gannon": {
+#                                "HP" : 100,
+#                                "Attack" : "Stomp"
+#                                },
+#                            "Link": {
+#                                "HP" : 100,
+#                                "Attack" : "Slash"
+#                                }
+#                            }
+#                    print("You have entered the boss room to fight Gannon!")
+#                    print(f"Gannon : {Gannon}")
+#                    print(f"Link : {Link}")
+
+            # getting to the last room that includes the triforce        
+            if (self.x, self.y) == triforce[0]:
+                print("You have found the epic Triforce!")
+                inventory.append("triforce")
+
+# defining rooms with keys
+keys = [(4, 4), (2, 0)]
+
+# defining the room with the triforce
+triforce = [(0, 0)]
+
+# defining the boss room
+boss = [(1, 0)]
+
+# defining all of the possible paths the player can take
+paths = [((1, 4), (2, 4)),
+         ((2, 4), (1, 4)),
+         ((1, 4), (0, 4)),
+         ((0, 4), (1, 4)),
+         ((0, 4), (0, 3)),
+         ((0, 3), (0, 4)),
+         ((0, 3), (1, 3)),
+         ((1, 3), (0, 3)),
+         ((1, 3), (2, 3)),
+         ((2, 3), (1, 3)),
+         ((2, 3), (2, 2)),
+         ((2, 2), (2, 3)),
+         ((2, 3), (3, 3)),
+         ((3, 3), (3, 4)),
+         ((3, 4), (4, 4)),
+         ((4, 4), (3, 4)),
+         ((3, 4), (3, 3)),
+         ((3, 3), (2, 3)),
+         ((2, 2), (3, 2)),
+         ((3, 2), (4, 2)),
+         ((4, 2), (4, 1)),
+         ((4, 1), (4, 2)),
+         ((4, 1), (4, 0)),
+         ((4, 0), (4, 1)),
+         ((4, 0), (3, 0)),
+         ((3, 0), (4, 0)),
+         ((3, 0), (2, 0)),
+         ((2, 0), (3, 0)),
+         ((2, 0), (1, 0)),
+         ((1, 0), (2, 0)),
+         ((0, 0), (1, 0)),
+         ((1, 0), (0, 0))]
+
+# defining the two paths with the locked door
+lockedDoors = [((2, 2), (2, 3)), ((1, 0), (0, 0))]
+
+# defining the map size, start point, and loading paths to "m"
+m = Map(5, 5, 2, 4, paths)
+
+# keeping the game going during each turn
+while True:
+    print("Inventory: ", inventory)
+    if "triforce" not in inventory:
+        print(zelda)
+        m.print_map()
+        direction = input("What direction do you want to move? [up, down, left, right] ")
+        m.move(direction)
+
+    # game ends when the "triforce" is collected
+    else:
+        print(gameover)
+        print("You have saved the princess and the kingdom of Hyrule by collecting the Triforce! You win!")
+        break
 # Add a moves counter
 # Have multiple items in the same room
 # Fight monsters
@@ -54,59 +225,6 @@ rooms = {
 # Only allow final boss fight when boss key is collected and all items collected
 # Get the triforce when beating the boss, automatically put player outside dungeon
 # start the player in the Hall
-currentRoom = 'Hall'
-
-showInstructions()
 
 # have 3 lives and a certain HP. Eat food to increase health
 # breaking this while loop means the game is over
-while True:
-    showStatus()
-
-    # the player MUST type something in
-    # otherwise input will keep asking
-    move = ''
-    while move == '':  
-        move = input('>')
-
-    # normalizing input:
-    # .lower() makes it lower case, .split() turns it to a list
-    # therefore, "get golden key" becomes ["get", "golden key"]          
-    move = move.lower().split(" ", 1)
-
-    #if they type 'go' first
-    if move[0] == 'go':
-        #check that they are allowed wherever they want to go
-        if move[1] in rooms[currentRoom]:
-            #set the current room to the new room
-            currentRoom = rooms[currentRoom][move[1]]
-        # if they aren't allowed to go that way:
-        else:
-            print('You can\'t go that way!')
-
-    #if they type 'get' first
-    if move[0] == 'get' :
-        # make two checks:
-        # 1. if the current room contains an item
-        # 2. if the item in the room matches the item the player wishes to get
-        if "item" in rooms[currentRoom] and move[1] in rooms[currentRoom]['item']:
-            #add the item to their inventory
-            inventory.append(move[1])
-            #display a helpful message
-            print(move[1] + ' got!')
-            #delete the item key:value pair from the room's dictionary
-            del rooms[currentRoom]['item']
-        # if there's no item in the room or the item doesn't match
-        else:
-            #tell them they can't get it
-            print('Can\'t get ' + move[1] + '!')
-        # start monster fight
-        # add attacks and turns for player
-        # get item after defeating a monster
-        ## If a player enters a room with a monster
-    if 'item' in rooms[currentRoom] and 'monster' in rooms[currentRoom]['item']:
-        print('A monster has got you... GAME OVER!')
-        break
-# create locked doors that only open with a key
-# develop a map to print out
-
